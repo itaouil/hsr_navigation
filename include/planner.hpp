776a8c1d223ea15200f1b3fc1a840b17145d30e0
Ilyass Taouil <itaouil95@gmail.com>
2020-03-22 23:38:03 +0100

    Changed to tf2 for dwa local planner.

--- include/planner.hpp
+++ include/planner.hpp
@@ -8,10 +8,10 @@
 #include "ros/ros.h"
 #include <nav_msgs/Path.h>
 #include <nav_msgs/GetMap.h>
-#include <tf/transform_listener.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <tf2_ros/transform_listener.h>
 #include <dwa_local_planner/dwa_planner_ros.h>
 #include <hsr_planner/ClutterPlannerService.h>
 
@@ -50,8 +50,8 @@ private:
     ros::NodeHandle m_nodeHandle;
     dwa_local_planner::DWAPlannerROS m_dp;
     nav_msgs::OccupancyGrid m_occupacyGrid;
-    boost::shared<tf::TransformListener> m_tf;
-    boost::shared<costmap_2d::Costmap2DROS> m_costMap;
+    boost::shared_ptr<tf2_ros::TransformListener> m_tf;
+    boost::shared_ptr<costmap_2d::Costmap2DROS> m_costMap;
 };
 
 #endif // PLANNER_HPP_
