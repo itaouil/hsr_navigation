4ef1e197c5140b36f2cd78ac5ebd59fe34a1177f
Ilyass Taouil <itaouil95@gmail.com>
2020-04-01 13:27:29 +0200

    AMCL added and debugging localization issues.

--- src/planner.cpp
+++ src/planner.cpp
@@ -36,18 +36,18 @@ Planner::~Planner()
 void Planner::initialize()
 {
     // Create path publisher
-    m_pub = m_nodeHandle.advertise<nav_msgs::Path>("/service_path", 1);
+    m_pub = m_nodeHandle.advertise<nav_msgs::Path>("/service_path", 1000);
 
     // Create velocity publisher
-    m_velPub = m_nodeHandle.advertise<geometry_msgs::Twist>("/hsrb/command_velocity", 1);
+    m_velPub = m_nodeHandle.advertise<geometry_msgs::Twist>("/hsrb/command_velocity", 1000);
 
     // Initialize costmaps (global and local)
     m_local = new costmap_2d::Costmap2DROS("local_costmap", m_buffer);
     m_global = new costmap_2d::Costmap2DROS("global_costmap", m_buffer);
 
     // Start costmaps
-    m_local->start();
-    m_global->start();
+    //m_local->start();
+    //m_global->start();
 }
 
 /**
@@ -116,14 +116,23 @@ void Planner::populatePlannerRequest(hsr_planner::ClutterPlannerService &p_servi
 
     // Start pose
     geometry_msgs::PoseStamped l_start;
+    // l_start.header.frame_id = "map";
+	// l_start.pose.position.x = l_global_pose.pose.position.x;
+	// l_start.pose.position.y = l_global_pose.pose.position.y;
+    // l_start.pose.position.z = l_global_pose.pose.position.z;
+    // l_start.pose.orientation.x = l_global_pose.pose.orientation.x;
+    // l_start.pose.orientation.y = l_global_pose.pose.orientation.y;
+    // l_start.pose.orientation.z = l_global_pose.pose.orientation.z;
+	// l_start.pose.orientation.w = l_global_pose.pose.orientation.w;
+
     l_start.header.frame_id = "map";
-	l_start.pose.position.x = l_global_pose.pose.position.x;
-	l_start.pose.position.y = l_global_pose.pose.position.y;
-    l_start.pose.position.z = l_global_pose.pose.position.z;
-    l_start.pose.orientation.x = l_global_pose.pose.orientation.x;
-    l_start.pose.orientation.y = l_global_pose.pose.orientation.y;
-    l_start.pose.orientation.z = l_global_pose.pose.orientation.z;
-	l_start.pose.orientation.w = l_global_pose.pose.orientation.w;
+    l_start.pose.position.x = 0;
+	l_start.pose.position.y = 0;
+    l_start.pose.position.z = 0;
+    l_start.pose.orientation.x = 0;
+    l_start.pose.orientation.y = 0;
+    l_start.pose.orientation.z = 0;
+	l_start.pose.orientation.w = 0;
 
     // Goal pose
     geometry_msgs::PoseStamped l_goal;
@@ -201,8 +210,8 @@ void Planner::dwaTrajectoryControl(const hsr_planner::ClutterPlannerService &p_s
     while (!m_dp.isGoalReached())
     {
         // Update costmaps
-        m_local->updateMap();
-        m_global->updateMap();
+        // m_local->updateMap();
+        // m_global->updateMap();
 
         // Compute velocity commands
         if (m_dp.computeVelocityCommands(l_cmd_vel))
