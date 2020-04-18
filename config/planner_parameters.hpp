85b6f5ca7f045e44cbc9ab028b69e1959c6e1c37
Ilyass Taouil <itaouil95@gmail.com>
2020-04-18 18:09:39 +0200

    Changed parameters for the planner.

--- config/planner_parameters.hpp
+++ config/planner_parameters.hpp
@@ -4,7 +4,7 @@
 bool BACKUPPLAN = false;
 float ROBOTRADIUS = 0.215;
 unsigned int OBJMOVCOST = 2;
-float INFLATIONRADIUS = 0.4;
+float INFLATIONRADIUS = 0.8;
 unsigned int MOVINGCOST = 11;
 unsigned int ENTERCOST = 100;
 unsigned int CHANGEOBJECTCOST = 250;
