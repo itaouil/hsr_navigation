6f10b60e95427467f08bd6460f73ff1ecc21219e
lilli <brucksch@cs.uni-bonn.de>
2020-06-01 11:38:46 +0200

    Modified script for grasping.

--- scripts/grasp.py
+++ scripts/grasp.py
@@ -6,14 +6,11 @@ import rospy
 import sys
 from hsrb_interface import geometry
 
-# Move timeout[s]
-_MOVE_TIMEOUT = 60.0
-
 # Grasp force[N]
 _GRASP_FORCE = 0.2
 
-# TF name of the bottle
-_BOTTLE_TF = 'ar_marker/4000'
+# TF name of the object to be grasped
+_BOTTLE_TF = 'grasp_frame'
 
 # TF name of the gripper
 _HAND_TF = 'hand_palm_link'
@@ -24,58 +21,62 @@ gripper = robot.get('gripper')
 omni_base = robot.get('omni_base')
 whole_body = robot.get('whole_body')
 
-# Posture that 0.02[m] front and rotate -1.57 around z-axis of the object
-bottle_to_hand = geometry.pose(z=-0.02, ek=-1.57)
-
-# Posture to move the hand 0.1[m] up
-hand_up = geometry.pose(x=0.1)
-
-# Posture to move the hand 0.5[m] back
-hand_back = geometry.pose(z=-0.5)
-
-# Location of the sofa
-sofa_pos = (1.2, 0.4, 1.57)
-
 if __name__=='__main__':
     try:
-        gripper.command(1.0)
-        whole_body.move_to_go()
-    except:
-        rospy.logerr('Failed to initialize')
-        sys.exit()
+        # Command to open the gripper
+        gripper.command(1.2)
+        print("Done0")
 
-    try:
-        # Move to the location where the bottle is viewable
-        omni_base.go_abs(sofa_pos[0], sofa_pos[1], sofa_pos[2], _MOVE_TIMEOUT)
-    except:
-        rospy.logerr('fail to move')
-        sys.exit()
-
-    try:
         # Transit to initial grasping posture
         whole_body.move_to_neutral()
+        print("Done1")
 
         # Look at the hand after the transition
         whole_body.looking_hand_constraint = True
+        print("Done2")
 
-        # Move the hand to front of the bottle
-        whole_body.move_end_effector_pose(bottle_to_hand, _BOTTLE_TF)
+        # First approach grasping
+        whole_body.move_end_effector_pose(geometry.pose(x=0.1, z=-0.1), _BOTTLE_TF)
+        print("Done3a")
+
+        # Second approach grasping
+        whole_body.move_end_effector_pose(geometry.pose(x=0.1, z=-0.02), _BOTTLE_TF)
+        print("Done3b")
 
         # Specify the force to grasp
         gripper.apply_force(_GRASP_FORCE)
+        print("Done4")
 
         # Wait time for simulator's grasp hack. Not needed on actual robot
         rospy.sleep(2.0)
+        print("Done5")
 
-        # Move the hand up on end effector coordinate
-        whole_body.move_end_effector_pose(hand_up, _HAND_TF)
-
-        # Move the hand back on end effector coordinate
-        whole_body.move_end_effector_pose(hand_back, _HAND_TF)
-
-        # Transit to initial posture
+        # Transit to neutral position
         whole_body.move_to_neutral()
+        print("Done8")
 
-    except:
+        # Rotate 180 degrees
+        omni_base.go_rel(0.0, 0.0, 3.14)
+        print("Done9")
+
+        # Release object
+        gripper.command(1.2)
+        rospy.sleep(2.0)
+        print("Done10")
+
+        # Rotate to initial pose
+        omni_base.go_rel(0.0, 0.0, 3.14)
+        print("Done12")
+
+        # Close gripper
+        gripper.command(0.0)
+        print("Done11")
+
+        # Switch to moving configuration
+        whole_body.move_to_go()
+        print("Done12")
+
+    except Exception as e:
+        print("Exception occured: ", e)
         rospy.logerr('Failed to grasp')
         sys.exit()
