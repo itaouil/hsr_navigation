3b3ea8b6cc1acd1cb79d97f301ec2146782beb7c
Ilyass Taouil <itaouil95@gmail.com>
2020-06-05 18:35:30 +0200

    Missing header.

--- include/control.hpp
+++ include/control.hpp
@@ -22,6 +22,7 @@
 #include <tf/transform_datatypes.h>
 #include <tf/transform_broadcaster.h>
 #include <costmap_2d/costmap_2d_ros.h>
+#include <tf2_ros/transform_listener.h>
 #include <dwa_local_planner/dwa_planner_ros.h>
 
 // Config file with paramters
